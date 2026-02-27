887ef27ffb3569ab9048dc94bf88fe5550d624ad
Ben Martinson <benmartinson92@gmail.com>
2026-02-27 13:14:03 -0800

    prod urls

--- app.config.js
+++ app.config.js
@@ -35,13 +35,14 @@ export default {
         projectId: "5125c995-e30c-4914-a96f-ec9c59acea81",
       },
       // Development URLs - for local testing
-      // devBaseUrl: "http://192.168.1.124:8000",
-      // devWsUrl: "ws://192.168.1.124:8000/ws/transcribe",
-      devBaseUrl: "https://aqgubuisev.us-west-2.awsapprunner.com",
-      devWsUrl: "wss://aqgubuisev.us-west-2.awsapprunner.com/ws/transcribe",
+      devBaseUrl: "http://192.168.1.124:8000",
+      devWsUrl: "ws://192.168.1.124:8000/ws/transcribe",
+      // devBaseUrl: "https://aqgubuisev.us-west-2.awsapprunner.com",
+      // devWsUrl: "wss://aqgubuisev.us-west-2.awsapprunner.com/ws/transcribe",
       // Production URLs - set via EAS environment variables or replace with your actual URLs
-      productionBaseUrl: process.env.PRODUCTION_BASE_URL,
-      productionWsUrl: process.env.PRODUCTION_WS_URL,
+      productionBaseUrl: "https://aqgubuisev.us-west-2.awsapprunner.com",
+      productionWsUrl:
+        "wss://aqgubuisev.us-west-2.awsapprunner.com/ws/transcribe",
     },
     owner: "benmartinson92",
   },
