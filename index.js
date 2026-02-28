10d368ec5dff4f8666a014ca1aba247f07dd5eb7
Ben Martinson <benmartinson92@gmail.com>
2026-02-27 16:57:25 -0800

    build

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
+import "react-native-gesture-handler";
 import { registerRootComponent } from "expo";
 
-try {
-  const App = require("./App").default;
-  registerRootComponent(App);
-} catch (e) {
-  const { View, Text } = require("react-native");
-  const ErrorApp = () => (
-    <View style={{ flex: 1, justifyContent: "center", padding: 20 }}>
-      <Text selectable>{String(e)}</Text>
-    </View>
-  );
-  registerRootComponent(ErrorApp);
-}
+const App = require("./App").default;
+registerRootComponent(App);
