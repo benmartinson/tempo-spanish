13d3f82b340f1ad5ea421657b25c6464b5fe7012
Ben Martinson <benmartinson92@gmail.com>
2026-04-09 11:20:04 -0700

    switch rn-starter to tempo-spanish

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 export default {
   expo: {
-    name: "rn-starter",
-    slug: "rn-starter",
+    name: "Tempo Spanish",
+    slug: "tempo-spanish",
     version: "1.0.0",
     orientation: "portrait",
     icon: "./assets/icon.png",
