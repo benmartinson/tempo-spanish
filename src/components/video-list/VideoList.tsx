d3390deff05167184e3e5d4492156df6847b4407
Ben Martinson <benmartinson92@gmail.com>
2026-04-20 16:50:03 -0700

    sort vids by release date

--- src/components/video-list/VideoList.tsx
+++ src/components/video-list/VideoList.tsx
@@ -165,8 +165,8 @@ const VideoList: React.FC = () => {
       .filter((video) => video.channel_id === selectedChannel.channel_id)
       .sort(
         (a, b) =>
-          new Date(b.created_at ?? 0).getTime() -
-          new Date(a.created_at ?? 0).getTime(),
+          new Date(b.release_date ?? b.created_at ?? 0).getTime() -
+          new Date(a.release_date ?? a.created_at ?? 0).getTime(),
       );
     return (
       <ChannelVideoList
